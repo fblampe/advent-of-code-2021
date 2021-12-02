b828b87fce047c08f6f502c2ce4f93be751cc1fc
Felix Lampe <mail@flampe.de>
2021-12-02 22:25:19 +0100

    Day 2: implement part 2 and improve readability

--- day2/src/main/kotlin/Part2.kt
+++ day2/src/main/kotlin/Part2.kt
@@ -3,15 +3,19 @@ import java.io.File
 fun main() {
     var horizontal = 0
     var vertical = 0
-    val moves = File("input").readText().split("\n")
+    var aim = 0
+    val moves = File("inputSofia").readText().split("\n")
 
     moves.forEach {
         val direction = it.substringBefore(" ")
         val amount = it.substringAfter(" ").toInt()
         when (direction) {
-            "forward" -> horizontal += amount
-            "up" -> vertical -= amount
-            "down" -> vertical += amount
+            "forward" -> {
+                horizontal += amount
+                vertical += aim * amount
+            }
+            "up" -> aim -= amount
+            "down" -> aim += amount
         }
     }
 
