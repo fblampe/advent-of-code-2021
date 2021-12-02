5797394d86f395bd451d0253165c49235d19ddc8
Felix Lampe <mail@flampe.de>
2021-12-02 21:18:19 +0100

    Day 1: replace for loop with functional impl

--- day1/kotlin/src/main/kotlin/Main.kt
+++ day1/kotlin/src/main/kotlin/Main.kt
@@ -3,12 +3,7 @@ import java.io.File
 fun main() {
     val numbers = File("input").readText().split("\n").map { it.toInt() }
 
-    var previous: Int? = null
-    var increaseCount = 0
-    for (number: Int in numbers) {
-        if (previous != null && number > previous) increaseCount++
-        previous = number
-    }
+    val result = numbers.zipWithNext().filter { it.second > it.first }.count()
 
-    println(increaseCount)
+    println(result)
 }
