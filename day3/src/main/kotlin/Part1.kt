3bd87b6393068581a090e01451d00111585940ad
Felix Lampe <mail@flampe.de>
2021-12-05 01:26:28 +0100

    Day 3: Refactor Part 1 for reuse in Part 2

--- day3/src/main/kotlin/Part1.kt
+++ day3/src/main/kotlin/Part1.kt
@@ -3,18 +3,27 @@ import java.io.File
 fun main() {
     val bitStrings = File("input").readText().split("\n")
 
-    val listsOfBitsAsInts = bitStrings.map { bitString -> bitString.toCharArray().map { it.toString().toInt() } }
+    val listsOfBitsAsInts = bitStrings.toListOfIntBits()
 
-    val sums = listsOfBitsAsInts.sumVertically()
-
-    val gamma = sums.map { if (it > 500) 1 else 0 }.joinToString("").toInt(2)
-    val epsilon = sums.map { if (it < 500) 1 else 0 }.joinToString("").toInt(2)
+    val gamma = listsOfBitsAsInts.mostCommonBitValues().toDecimalInt()
+    val epsilon = listsOfBitsAsInts.leastCommonBitValues().toDecimalInt()
 
     println("gamma: $gamma")
     println("epsilon: $epsilon")
     println("answer: ${gamma * epsilon}")
 }
 
-fun List<List<Int>>.sumVertically(): List<Int> = fold(arrayOfNulls<Int>(12).map { 0 }) { acc: List<Int>, bits ->
-    acc.zip(bits).map { (x, y) -> x + y }
-}
+fun List<String>.toListOfIntBits() = map { bitString -> bitString.toCharArray().map { it.toString().toInt() } }
+
+fun List<List<Int>>.mostCommonBitValues() =
+    sumVertically().map { if (it >= size / 2.0) 1 else 0 }
+
+fun List<List<Int>>.leastCommonBitValues() =
+    sumVertically().map { if (it < size / 2.0) 1 else 0 }
+
+fun List<List<Int>>.sumVertically(): List<Int> =
+    fold(arrayOfNulls<Int>(this.first().size).map { 0 }) { acc: List<Int>, bits ->
+        acc.zip(bits).map { (x, y) -> x + y }
+    }
+
+fun List<Int>.toDecimalInt() = joinToString("").toInt(2)
